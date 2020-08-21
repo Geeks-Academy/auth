3057be1198c0d5f54ad7b67a7f112804c4b21cc6
Kacper Małachowski <kacpermaachowski@gmail.com>
2020-08-21 13:36:56 +0200

    Initialize passport and fix undefiend in start message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const config = require('./config');
 const routes = require('./routes');
 const mongo = require('./mongodb');
+require('./config/passport');
+
+console.log(config.serverOptions.port)
 
 mongo.connect(config.mongo.connectionString, config.mongo.options);
 
@@ -10,9 +14,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
 
 routes(app);
 
 app.listen(config.serverOptions.port, () => {
-  console.log(`Server is running at: ${config.port}`);
+  console.log(`Server is running at: ${config.serverOptions.port}`);
 })
