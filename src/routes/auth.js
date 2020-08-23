59af232f002361bf2b683cc731180d032832230c
Kacper Małachowski <kacpermaachowski@gmail.com>
2020-08-23 08:55:58 +0200

    #AA8K-3 Move /callback route under / in auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const passport = require('passport');
 const AuthController = require('../controllers/AuthController');
 
-router.get('/callback', passport.authenticate('github'), AuthController.redirectUser)
-
 router.get('/', passport.authenticate('github'));
+
+router.get('/callback', passport.authenticate('github'), AuthController.redirectUser)
 
 module.exports = router;
