948d38f403fe49e3156a42c2db28209f8b09949e
Kacper Małachowski <kacpermaachowski@gmail.com>
2020-08-31 10:17:54 +0200

    #AA8K-2 Refactor HealthCheckControlleer to typescritp and add tests

---
+++ src/__tests__/HealthCheckController.spec.ts
@@ -0,0 +1,110 @@
+import HealthCheckController from '../controllers/HealthCheckController';
+import mongoose from 'mongoose';
+
+jest.mock('mongoose');
+let connection: mongoose.Connection;
+
+describe('HealthCheckController', () => {
+  beforeEach(() => {
+    connection = new mongoose.Connection(mongoose);
+  });
+
+  it.each`
+    status | expected | readyString    | dbStateString
+    ${0}   | ${false} | ${'not ready'} | ${'diconnected from'}
+    ${1}   | ${true}  | ${'ready'}     | ${'connected to'}
+    ${2}   | ${false} | ${'not ready'} | ${'connecting to'}
+    ${3}   | ${false} | ${'not ready'} | ${'diconnecting from'}
+  `('is $readyString when $dbStateString DB', ({ status, expected }) => {
+    connection.readyState = status;
+    const healthCheckController: HealthCheckController = new HealthCheckController(
+      connection,
+    );
+
+    const result = healthCheckController.getServiceStatus();
+
+    expect(result.service.isReady).toBe(expected);
+  });
+
+  it.each([
+    [0, 0],
+    [1, 1],
+    [2, 2],
+    [3, 3],
+  ])(
+    `return database status (provided: %i) same as mongoose readyState (expected: %i)`,
+    (provided, expected) => {
+      connection.readyState = provided;
+      const healthCheckController: HealthCheckController = new HealthCheckController(
+        connection,
+      );
+
+      const result = healthCheckController.getServiceStatus();
+      expect(result.database.status).toBe(expected);
+    },
+  );
+
+  it.each`
+    status | expected | readyString    | dbStateString
+    ${0}   | ${0}     | ${'not ready'} | ${'diconnected from'}
+    ${1}   | ${1}     | ${'ready'}     | ${'connected to'}
+    ${2}   | ${0}     | ${'not ready'} | ${'connecting to'}
+    ${3}   | ${0}     | ${'not ready'} | ${'diconnecting from'}
+  `(
+    'has $readyString (numeric value: $expected) status when $dbStateString DB',
+    ({ status, expected }) => {
+      connection.readyState = status;
+      const healthCheckController: HealthCheckController = new HealthCheckController(
+        connection,
+      );
+
+      const result = healthCheckController.getServiceStatus();
+
+      expect(result.service.status).toBe(expected);
+    },
+  );
+
+  it.each`
+    status | changedTo | expected | readyString    | dbStateString      | newDBStateString
+    ${0}   | ${1}      | ${true}  | ${'ready'}     | ${'diconnected'}   | ${'connected'}
+    ${1}   | ${2}      | ${false} | ${'not ready'} | ${'connected'}     | ${'connecting'}
+    ${2}   | ${3}      | ${false} | ${'not ready'} | ${'connecting'}    | ${'diconnecting'}
+    ${3}   | ${0}      | ${false} | ${'not ready'} | ${'disconnecting'} | ${'diconnected'}
+  `(
+    'return $expected when DB connection state changed from $dbStateString to $newDBStateString',
+    ({ status, changedTo, expected }) => {
+      connection.readyState = status;
+      const healthCheckController: HealthCheckController = new HealthCheckController(
+        connection,
+      );
+
+      connection.readyState = changedTo;
+
+      const result = healthCheckController.getServiceStatus();
+
+      expect(result.service.isReady).toBe(expected);
+    },
+  );
+
+  it.each`
+    status | changedTo | expected | readyString    | dbStateString      | newDBStateString
+    ${0}   | ${1}      | ${1}     | ${'ready'}     | ${'diconnected'}   | ${'connected'}
+    ${1}   | ${2}      | ${2}     | ${'not ready'} | ${'connected'}     | ${'connecting'}
+    ${2}   | ${3}      | ${3}     | ${'not ready'} | ${'connecting'}    | ${'diconnecting'}
+    ${3}   | ${0}      | ${0}     | ${'not ready'} | ${'disconnecting'} | ${'diconnected'}
+  `(
+    'return $expected numeric DB connection state when it changed from $dbStateString (numeric: $status) to $newDBStateString (numeric: $changedTo)',
+    ({ status, changedTo, expected }) => {
+      connection.readyState = status;
+      const healthCheckController: HealthCheckController = new HealthCheckController(
+        connection,
+      );
+
+      connection.readyState = changedTo;
+
+      const result = healthCheckController.getServiceStatus();
+
+      expect(result.database.status).toBe(expected);
+    },
+  );
+});
---
+++ src/controllers/HealthCheckController.ts
@@ -0,0 +1,69 @@
+import { Connection } from 'mongoose';
+
+export interface IResult {
+  database: {
+    isConnected: boolean;
+    status: number;
+    message: string;
+  };
+  service: {
+    isReady: boolean;
+    status: number;
+    message: string;
+  };
+}
+
+export default class HealthCheckController {
+  constructor(private connection: Connection) {}
+
+  public getServiceStatus(): IResult {
+    return {
+      database: {
+        isConnected: this.isConnected(),
+        status: this.connection.readyState,
+        message: this.databaseStatusMessage(),
+      },
+      service: {
+        isReady: this.isConnected(),
+        status: this.isConnected() ? 1 : 0,
+        message: 'Test',
+      },
+    };
+  }
+
+  private serviceStatusMessage() {
+    switch (this.connection.readyState) {
+      case 0 || 2 || 3:
+        return 'Not ready yet!';
+
+      case 1:
+        return 'Ready!';
+
+      default:
+        return 'Unknow service status!';
+    }
+  }
+
+  private databaseStatusMessage(): string {
+    switch (this.connection.readyState) {
+      case 0:
+        return 'Diconnected from database';
+
+      case 1:
+        return 'Connected to database';
+
+      case 2:
+        return 'Connecting to database...';
+
+      case 3:
+        return ' Disconecting from database...';
+
+      default:
+        return 'Unknown database connection state!';
+    }
+  }
+
+  private isConnected() {
+    return this.connection.readyState == 1;
+  }
+}
