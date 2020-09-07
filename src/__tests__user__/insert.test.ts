cf2d30de44c0ec281efc03c68ab670f3e1d69fd7
Bartlomiej Figatowski <bartek_19_83@hotmail.com>
2020-09-07 18:17:23 -0400

    change mongodb schema to mongoose.ts/ wrote test for mongoose schema

--- src/__tests__user__/insert.test.ts
+++ src/__tests__user__/insert.test.ts
@@ -1,22 +1,44 @@
-import { createOneDoc, findOneDoc } from "../crud__operation";
-import { User } from "../models/User";
+import mongoose from "mongoose";
+import User, { IUser } from "../models/User";
 
-describe("insert", () => {
-  it("should insert a doc into collection", async () => {
-    const mockUser: User = new User(
-      "7BNE8r{?3Xn5~xF7",
-      "John",
-      "%~eGJ9!!DWy=4KA4"
-    );
-    await createOneDoc(mockUser);
-
-    const query: object = { username: "John" };
-    const insertedUser = await findOneDoc(query);
-    expect(insertedUser);
+describe("User model", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   });
-  it("should not receive the document", async () => {
-    const query: object = { username: "Jackson" };
-    const insertedUser = await findOneDoc(query);
-    expect(insertedUser);
+
+  afterAll(async () => {
+    mongoose.connection.close();
+  });
+
+  it("Should throw validation errors", () => {
+    const user = new User();
+
+    expect(user.validate).toThrow();
+  });
+
+  it("Should save a user", async () => {
+    expect.assertions(3);
+
+    const user: IUser = new User({
+      githubId: "Test github Id",
+      username: "Test username",
+      token: "Test token",
+    });
+    const spy = jest.spyOn(user, "save");
+
+    user.save();
+
+    expect(spy).toHaveBeenCalled();
+
+    expect(user).toMatchObject({
+      githubId: expect.any(String),
+      username: expect.any(String),
+      token: expect.any(String),
+    });
+
+    expect(user.token).toBe("Test token");
   });
 });
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,17 +1,16 @@
-export class User {
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
   githubId: string;
   username: string;
   token: string;
-  created: Date;
-  constructor(
-    githubId: string,
-    username: string,
-    token: string,
-    created = new Date()
-  ) {
-    this.githubId = githubId;
-    this.username = username;
-    this.token = token;
-    this.created = created;
-  }
 }
+
+const UserSchema: Schema = new Schema({
+  githubId: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  token: { type: String, required: true },
+});
+
+// Export the model and return your IUser interface
+export default mongoose.model<IUser>("User", UserSchema);
