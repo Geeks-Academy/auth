6b8c064f01aebeb64a0141bdef38d9e280852d70
Kacper Malachowski <kacpermaachowski@gmail.com>
2020-09-12 17:57:24 +0200

    AA8K-9 Add routes definition in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import { Express, default as express } from 'express';
 import cors from 'cors';
+import routes from './routes';
 
 export default function initialize(app: Express): Express {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cors);
+
+  app.use(routes);
 
   return app;
 }
