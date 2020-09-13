ea2559dd82fad2682017fa3b42dfeb8e71762a8a
Kacper Malachowski <kacpermaachowski@gmail.com>
2020-09-13 10:17:12 +0200

    AA8K-9 - Configure passport.js to use before rewrite

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { Express, default as express } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import passport from 'passport';
+
+require('./config/passport.js');
 
 const initialize = (): Express => {
   const app = express();
@@ -8,6 +11,9 @@ const initialize = (): Express => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cors());
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.use(routes());
 
--- src/config/passport.js
+++ src/config/passport.js
@@ -1,25 +1,25 @@
 const passport = require('passport');
 const GithubStrategy = require('passport-github').Strategy;
-const User = require('../models/User');
+const { User } = require('../models/User');
 const config = require('./index');
 
-passport.use(new GithubStrategy(config.githubOAuth, 
-  (accessToken, refreshToken, profile, cb) => {
-    User
-      .findOneOrCreate(accessToken, profile)
-      .then((user) => {
-        if(user) 
-          cb(null, user);
-        else
-          cb();
-      })
-}))
+passport.use(
+  new GithubStrategy(
+    config.githubOAuth,
+    (accessToken, refreshToken, profile, cb) => {
+      User.findOneOrCreate(accessToken, profile).then((user) => {
+        if (user) cb(null, user);
+        else cb();
+      });
+    },
+  ),
+);
 
 passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser((userID, done) => {
   User.findOne({ id: userID }, (err, user) => {
-    if(err) return done(err);
+    if (err) return done(err);
     done(null, user);
-  })
+  });
 });
