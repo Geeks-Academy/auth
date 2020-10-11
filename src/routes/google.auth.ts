b2fb08cba53f5f01ee2a78005dbd1b23ae048a21
Hayuna <marek.kowalonek@gmail.com>
2020-10-11 13:10:30 +0200

    AA8K-17 - Increase ram

--- src/routes/google.auth.ts
+++ src/routes/google.auth.ts
@@ -25,46 +25,46 @@ getParametersFromAWS();
 
 const googleRoute = (app: Express): void => {
 
-let userProfile: GoogleUser;
+  let userProfile: GoogleUser;
 
-app.use(passport.initialize());
-app.use(passport.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
+  passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
 
-console.log(process.env)
-const GoogleStrategyOAuth2Strategy = GoogleStrategy.OAuth2Strategy;
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
-const GOOGLE_CALLBACKURL = process.env.GOOGLE_CALLBACKURL as string;
-passport.use(new GoogleStrategyOAuth2Strategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: GOOGLE_CALLBACKURL
-  },
-  (accessToken: string, 
-    refreshToken: string, 
-    profile: Profile, 
-    done: (info: null, userProfile: GoogleUser
-    ) => void) => {
+  console.log(process.env)
+  const GoogleStrategyOAuth2Strategy = GoogleStrategy.OAuth2Strategy;
+  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
+  const GOOGLE_CALLBACKURL = process.env.GOOGLE_CALLBACKURL as string;
+  passport.use(new GoogleStrategyOAuth2Strategy({
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACKURL
+    },
+    (accessToken: string, 
+      refreshToken: string, 
+      profile: Profile, 
+      done: (info: null, userProfile: GoogleUser
+      ) => void) => {
 
-      userProfile = profile as GoogleUser;
-      return done(null, userProfile);
-  }
-));
- 
-app.get('/auth/google', 
-  passport.authenticate('google', { scope : ['profile', 'email'] }));
- 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  (req, res) => userController.loginUserViaGoogle(userProfile, req, res)  );
+        userProfile = profile as GoogleUser;
+        return done(null, userProfile);
+    }
+  ));
+  
+  app.get('/auth/google', 
+    passport.authenticate('google', { scope : ['profile', 'email'] }));
+  
+  app.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/error' }),
+    (req, res) => userController.loginUserViaGoogle(userProfile, req, res)  );
 
 };
 
