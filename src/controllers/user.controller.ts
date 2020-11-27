ff52bbf2b3651dc96b031e21c5e8338cf5396f4e
Lukasz Hendrysiak <l.hendrysiak@optimumpareto.com>
2020-11-27 16:01:51 +0100

    AA8K-22 - Implemented merge user feature

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { ExtendedRequest } from 'models/extendedRequest';
 import { GithubUser, GoogleUser, IUserAttached } from 'models/user/user.model';
 import { UserService } from 'services/user.service';
 
@@ -16,24 +15,20 @@ const getUserById = async (id: string): Promise<IUserAttached | null> => {
   return await UserService.getUserById(id);
 };
 
-export const getUserData = async (req: ExtendedRequest, res: Response): Promise<void> => {
-  if (req.currentUser?.id) {
-    const user = await getUserById(req.currentUser?.id);
-    res.send(user);
-  }
-
+export const getUserData = async (req: Request, res: Response): Promise<void> => {
+  const user = await getUserByEmail(req.body?.email);
+  if (user) res.send(user);
   res.status(401).send('User does not exist!');
 };
 
-export const mergeUser = async (req: ExtendedRequest, res: Response) => {
+export const mergeUser = async (req: Request, res: Response) => {
   await UserService.saveUserData({
-    email: req.currentUser?.email, 
+    email: req.body.email, 
     lastLoggedIn: new Date(), 
-    repoUrl: req.currentUser?.repoUrl
   });
 };
 
-export const loginUserViaGoogle = async (verifiedUser: GoogleUser, req: ExtendedRequest, res: Response): Promise<void> => {
+export const loginUserViaGoogle = async (verifiedUser: GoogleUser, req: Request, res: Response): Promise<void> => {
 
   if (!verifiedUser) res.status(401).send('User does not exist!');
 
@@ -41,15 +36,13 @@ export const loginUserViaGoogle = async (verifiedUser: GoogleUser, req: Extended
 
   if (!user) {
     const savedUser = await UserService.saveUserData({email: verifiedUser.emails[0].value, lastLoggedIn: new Date()});
-    req.currentUser = savedUser;
     res.send(savedUser);
   } 
 
-  req.currentUser = user;
   res.send(user)
 };
 
-export const loginUserViaGithub = async (verifiedUser: GithubUser, req: ExtendedRequest, res: Response): Promise<void> => {
+export const loginUserViaGithub = async (verifiedUser: GithubUser, req: Request, res: Response): Promise<void> => {
   if (!verifiedUser) res.status(401).send('User does not exist!');
 
   const userEmail = verifiedUser._json.email;
@@ -57,16 +50,13 @@ export const loginUserViaGithub = async (verifiedUser: GithubUser, req: Extended
 
   if (userEmail){ 
     user = await getUserByEmail(userEmail);
-    req.currentUser = user;
     res.sendStatus(303).send("User exist already in database - would you like to merge user data?");
   }
   if (!user) user = await getUserByRepoUrl(verifiedUser.profileUrl);
   if (!user) {
     const savedUser = await UserService.saveUserData({repoUrl: verifiedUser.profileUrl, lastLoggedIn: new Date()});
-    req.currentUser = savedUser;
     res.send(savedUser);
   } 
 
-  req.currentUser = user;
   res.send(user)
 };
--- src/models/extendedRequest.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { IUserAttached } from './user/user.model';
-
-export interface ExtendedRequest extends Request {
-  currentUser?: IUserAttached | null;
-}
-
-export type ExtendedRequestHandler = (req: ExtendedRequest, res: Response, next: NextFunction) => Promise<void>;
